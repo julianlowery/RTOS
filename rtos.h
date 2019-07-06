d550a18ba287a992f194fcfec1ca7b927e40ba96
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-06 17:41:56 -0400

    Added context switch and include guards

--- rtos.h
+++ rtos.h
@@ -1,7 +1,15 @@
-#include "globals.h"
+#ifndef __rtos_h
+#define __rtos_h
 
+#include <stdio.h>
+#include <cmsis_armcc.h>
+
+#include "globals.h"
+#include "context.h"
+
+// Interface Functions
 void rtos_init(void);
-void task_create(void);
+void task_create(rtosTaskFunc_t function_pointer, void* function_arg, priority_t task_priority);
 
 void semaphore_create(void);
 void semaphore_take(void);
@@ -10,3 +18,8 @@ void semaphore_give(void);
 void mutex_create(void);
 void mutex_give(void);
 void mutex_take(void);
+
+// Other functions
+void context_switch(tcb_t *old_task, tcb_t *new_task);
+
+#endif //__rtos_h
