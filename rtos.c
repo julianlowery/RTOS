a38569aae5ef883667492d142cfaf8aa63754adf
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-05 20:55:29 -0400

    Finished rtos_init function and tested all operations

--- rtos.c
+++ rtos.c
@@ -1,45 +1,39 @@
 #include "rtos.h"
 
-tcb_t tcb1;
-tcb_t tcb2;
-tcb_t tcb3;
-tcb_t tcb4;
-tcb_t tcb5;
-tcb_t tcb6;
+tcb_t tcb_array[6];
 tcb_t tcb_main;
 
 uint8_t num_tasks = 0;
 
 void rtos_init(){
+	const uint32_t stack_size = 0x100;
+	const uint32_t num_tcbs = 6;
 	
 	// Initalize all task stack addresses (including main task stack)
-	int32_t *initial_sp_pointer = (int*)0x00000000; 
-	tcb_main.stack_base_address = (int*)(*initial_sp_pointer);
+	uint32_t *initial_sp_pointer = (uint32_t*)0x00000000; 
+	tcb_main.stack_base_address = (uint32_t*)(*initial_sp_pointer);
+	tcb_main.stack_overflow_address = tcb_main.stack_base_address-(stack_size*2);
 	
-	tcb1.stack_base_address = tcb_main.stack_base_address-0x0800;
-	tcb1.stack_overflow_address = tcb_main.stack_base_address-0x0C00;
-	
-	tcb2.stack_base_address = tcb_main.stack_base_address-0x0C00;
-	tcb2.stack_overflow_address = tcb_main.stack_base_address-0x1000;
-	
-	tcb3.stack_base_address = tcb_main.stack_base_address-0x1000;
-	tcb3.stack_overflow_address = tcb_main.stack_base_address-0x1400;
-	
-	tcb4.stack_base_address = tcb_main.stack_base_address-0x1400;
-	tcb4.stack_overflow_address = tcb_main.stack_base_address-0x1800;
-	
-	tcb5.stack_base_address = tcb_main.stack_base_address-0x1800;
-	tcb5.stack_overflow_address = tcb_main.stack_base_address-0x1C00;
-	
-	tcb6.stack_base_address = tcb_main.stack_base_address-0x1C00;
-	tcb6.stack_overflow_address = tcb_main.stack_base_address-0x2000;
-	
-	
+	for(uint8_t tcb_num = 0; tcb_num < num_tcbs; tcb_num++){
+		tcb_array[tcb_num].stack_base_address = (tcb_main.stack_overflow_address)-(stack_size*(tcb_num));
+		tcb_array[tcb_num].stack_overflow_address = tcb_main.stack_overflow_address-(stack_size*(tcb_num+1));
+	}
 	
 	// Copy contents of MAIN stack to new main() process stack
-	
-	// Set main stack pointer to base address of main() stack WHY TO BASE ADDRESS? WHY NOT TO TOP OF MAIN STACK?
-	
+	uint32_t main_stack_top = __get_MSP();
+	for(uint32_t *main_loc = tcb_main.stack_base_address; (uint32_t)main_loc >= main_stack_top; main_loc--){
+		uint32_t *main_task_loc = main_loc-(stack_size*2);
+		*main_task_loc = *main_loc;
+	}
+
+	// Set main stack pointer to base address of main() stack 
+	__set_MSP((uint32_t)tcb_main.stack_base_address);
+
 	// Switch from using msp to psp
+	uint32_t control_reg = __get_CONTROL();
+	__set_CONTROL((uint32_t)(control_reg|=(1<<1)));
 	
+	// Set psp to top of main() stack
+	uint32_t main_proc_top = main_stack_top-(stack_size*2);
+	__set_PSP(main_proc_top);
 }
