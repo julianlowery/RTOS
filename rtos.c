8ead2a4ff2e6b5d97379ed1b810ddfcb8b46a02a
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-06 22:08:53 -0400

    Added task_create function

--- rtos.c
+++ rtos.c
@@ -38,18 +38,61 @@ void rtos_init(){
 	__set_PSP(main_proc_top);
 }
 
-// Context Swtich - 	NEEDS TO BE TESTED ---------------------------------------------------------------------------
-void context_switch(tcb_t *old_task_tcb, tcb_t *new_task_tcb){
+bool context_switch(tcb_t *old_task_tcb, tcb_t *new_task_tcb){
+	
+	// Store old task registers on task stack
 	old_task_tcb->stack_pointer = (uint32_t*)storeContext();
 	
+	// Check for overflow on old task
+	if(old_task_tcb->stack_pointer <= old_task_tcb->stack_overflow_address)
+		return false;
+	
+	// Set psp to new task
 	uint32_t new_task_psp = (uint32_t)new_task_tcb->stack_pointer;
 	__set_PSP(new_task_psp);
 	
+	// Restore context of new task from  stack
 	restoreContext(new_task_psp);
+	return true;
+}
+
+bool push_to_stack(tcb_t *tcb, uint32_t value){
+	tcb->stack_pointer--;
+	
+	// Check for stack overflow
+	if(tcb->stack_pointer <= tcb->stack_overflow_address)
+		return false;
+	
+	*(tcb->stack_pointer) = value;
+	return true;
+}
+
+uint32_t pop_from_stack(tcb_t *tcb){
+	uint32_t returnValue = *(tcb->stack_pointer);
+	tcb->stack_pointer++;
+	
+	return returnValue;
 }
 
 void task_create(rtosTaskFunc_t function_pointer, void* function_arg, priority_t task_priority){
+	static uint8_t task_number = 0;
+	uint32_t default_psr_val = 0x01000000;
 	
+	tcb_array[task_number].task_id = task_number;
+	tcb_array[task_number].stack_pointer = tcb_array[task_number].stack_base_address;
+	tcb_array[task_number].state = READY;
+	tcb_array[task_number].priority = task_priority;
+	// HANDLE LIST POINTER HERE, SHOULD IT GO STRAIGHT TO READY LIST?
+	tcb_array[task_number].stack_size = 0; // not used yet
 	
+	// push all stack values incrementing psp
+	push_to_stack(&tcb_array[num_tasks], default_psr_val);
+	push_to_stack(&tcb_array[num_tasks], (uint32_t)function_pointer); // TEST THIS WELL
+	for(uint8_t count = 0; count < 5; count++)
+		push_to_stack(&tcb_array[num_tasks], 0x0);
+	push_to_stack(&tcb_array[num_tasks], (uint32_t)function_arg);
+	for(uint8_t count = 0; count < 8; count++)
+		push_to_stack(&tcb_array[num_tasks], 0x0);
 	
+	task_number++;
 }
