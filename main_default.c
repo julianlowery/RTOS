3ea2c898c6496224d233e95caf5ce18de0e0cedc
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-06 22:10:02 -0400

    Update main_default.c

--- main_default.c
+++ main_default.c
@@ -11,11 +11,18 @@ void SysTick_Handler(void) {
     msTicks++;
 }
 
+void task1(arg){
+	static int n = 0;
+	n++;
+}
+
 int main(void) {
 	SysTick_Config(SystemCoreClock/1000);
 	printf("\nStarting...\n\n");
 	
 	rtos_init();
+	
+	task_create(task1, NULL, LOW);
 	
 	uint32_t period = 1000; // 1s
 	uint32_t prev = -period;
@@ -26,3 +33,4 @@ int main(void) {
 		}
 	}
 }
+
