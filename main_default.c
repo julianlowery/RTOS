b1ed6833416372a558e4f5dd32ab988f96bc4a40
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-08 17:33:46 -0400

    Debugging

--- main_default.c
+++ main_default.c
@@ -6,28 +6,53 @@
 #include "rtos.h"
 
 uint32_t msTicks = 0;
+extern tcb_t tcb_array[6];
 
 void SysTick_Handler(void) {
     msTicks++;
 }
 
-
+/*
 void task1(void* arg){
 	static int n = 0;
 	n = (int)arg;
 	n++;
 }
 
+void task2(void* arg){
+	static int n = 0;
+	n = (int)arg;
+	n++;
+}
+
+void task3(void* arg){
+	static int n = 0;
+	n = (int)arg;
+	n++;
+}
+
+void task4(void* arg){
+	static int n = 0;
+	n = (int)arg;
+	n++;
+}
+*/
 
 int main(void) {
 	printf("\nStarting...\n\n");
 	SysTick_Config(SystemCoreClock/1000);
-	printf("\nStarting...\n\n");
+	printf("1\n");
 	
 	rtos_init();
 	
+	volatile int hello = 5;
+	volatile int hellos = 4;
+		/*
 	task_create(task1, NULL, LOW);
-	
+	task_create(task2, NULL, LOW);
+	task_create(task3, NULL, LOW);
+	task_create(task4, NULL, LOW);
+	*/
 	uint32_t period = 1000; // 1s
 	uint32_t prev = -period;
 	while(true) {
@@ -37,3 +62,4 @@ int main(void) {
 		}
 	}
 }
+
