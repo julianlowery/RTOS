6a930811bea65dfe8cfe1aa8a89d2734e2e980d1
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-10 17:15:42 -0400

    Setup and debugged systick for pendsv interrupt. Debugging context switch

--- rtos.c
+++ rtos.c
@@ -1,14 +1,13 @@
 #include "rtos.h"
-#include "context.h"
+#include "scheduler.h"
 #include <stdio.h>
-#include <LPC17xx.h>
 
 tcb_t tcb_array[6];
 tcb_t tcb_main;
 
 uint8_t num_tasks = 0;
 
-extern void SysTick_Handler(void); // find better solution?
+//extern void SysTick_Handler(void); // find better solution?
 
 void rtos_init(){
 	const uint32_t stack_size = 0x100; // Actaully physical stack_size/4
@@ -26,8 +25,6 @@ void rtos_init(){
 	tcb_array[main_task_num].priority = IDLE;
 	// HANDLE TCB LIST POINTER HERE, SHOULD IT GO STRAIGHT TO READY LIST?
 	tcb_array[main_task_num].stack_size = 0; // not used yet
-	
-	__disable_irq();
 	
 	// Initalize all task stack addresses (including main task stack)
 	uint32_t *initial_sp_pointer = (uint32_t*)vector_table_address; 
@@ -55,49 +52,11 @@ void rtos_init(){
 	// Set psp to top of main() stack (now task0 stack)
 	// multiplied by four because of division made earlier to account for 32 bit pointer incrementation
 	__set_PSP(*tcb_main.stack_pointer-(stack_size*2*4));
-	
+
 	// Switch from using msp to psp (local variables restored)
 	__set_CONTROL((uint32_t)__get_CONTROL|psp_enable);
 	
-	__enable_irq();
-}
 
-bool context_switch(tcb_t *old_task_tcb, tcb_t *new_task_tcb){
-	
-	// Store old task registers on task stack
-	old_task_tcb->stack_pointer = (uint32_t*)storeContext();
-	
-	// Check for overflow on old task
-	if(old_task_tcb->stack_pointer <= old_task_tcb->stack_overflow_address)
-		return false;
-	
-	// Set psp to new task
-	uint32_t new_task_psp = (uint32_t)new_task_tcb->stack_pointer;
-	__set_PSP(new_task_psp);
-	
-	// Restore context of new task from  stack
-	restoreContext(new_task_psp);
-	return true;
-}
-
-bool push_to_stack(tcb_t *tcb, uint32_t value){
-	tcb->stack_pointer--;
-	
-	// Check for stack overflow
-	if(tcb->stack_pointer <= tcb->stack_overflow_address){
-		printf("STACK OVERFLOW from task id: %d/n", tcb->task_id);
-		return false;
-	}
-	
-	*(tcb->stack_pointer) = value;
-	return true;
-}
-
-uint32_t pop_from_stack(tcb_t *tcb){
-	uint32_t returnValue = *(tcb->stack_pointer);
-	tcb->stack_pointer++;
-	
-	return returnValue;
 }
 
 void task_create(rtosTaskFunc_t function_pointer, void* function_arg, priority_t task_priority){
@@ -115,10 +74,11 @@ void task_create(rtosTaskFunc_t function_pointer, void* function_arg, priority_t
 	push_to_stack(&tcb_array[task_number], default_psr_val);
 	push_to_stack(&tcb_array[task_number], (uint32_t)function_pointer); // TEST THIS WELL
 	for(uint8_t count = 0; count < 5; count++)
-		push_to_stack(&tcb_array[task_number], 0x0);
+		push_to_stack(&tcb_array[task_number], 0x1);
 	push_to_stack(&tcb_array[task_number], (uint32_t)function_arg);
 	for(uint8_t count = 0; count < 8; count++)
-		push_to_stack(&tcb_array[task_number], 0x0);
+		push_to_stack(&tcb_array[task_number], 0x1);
 	
 	task_number++;
 }
+
