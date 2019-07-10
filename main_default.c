f7c5dbd7fac03001112121709e3e33b5fa8caa8a
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-09 22:47:30 -0400

    Debug updates, testing task_create

--- main_default.c
+++ main_default.c
@@ -12,13 +12,13 @@ void SysTick_Handler(void) {
     msTicks++;
 }
 
-/*
+
 void task1(void* arg){
 	static int n = 0;
 	n = (int)arg;
 	n++;
 }
-
+/*
 void task2(void* arg){
 	static int n = 0;
 	n = (int)arg;
@@ -39,20 +39,19 @@ void task4(void* arg){
 */
 
 int main(void) {
+	
 	printf("\nStarting...\n\n");
 	SysTick_Config(SystemCoreClock/1000);
 	printf("1\n");
 	
 	rtos_init();
-	
-	volatile int hello = 5;
-	volatile int hellos = 4;
-		/*
-	task_create(task1, NULL, LOW);
+
+	task_create(task1, (void*)0x11111111, LOW);
+/*
 	task_create(task2, NULL, LOW);
 	task_create(task3, NULL, LOW);
 	task_create(task4, NULL, LOW);
-	*/
+*/
 	uint32_t period = 1000; // 1s
 	uint32_t prev = -period;
 	while(true) {
