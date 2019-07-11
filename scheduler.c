31b504476e30ba5027db9cbfc9805bfe9ec8ad5f
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-10 22:50:08 -0400

    Debugging context switch in PendSV_Handler

--- scheduler.c
+++ scheduler.c
@@ -30,6 +30,8 @@ uint32_t pop_from_stack(tcb_t *tcb){
 
 bool context_switch(tcb_t *old_task_tcb, tcb_t *new_task_tcb){
 	
+	volatile uint32_t tester = 0x99999999;
+	
 	// Store old task registers on task stack
 	old_task_tcb->stack_pointer = (uint32_t*)storeContext();
 	
@@ -37,17 +39,22 @@ bool context_switch(tcb_t *old_task_tcb, tcb_t *new_task_tcb){
 	if(old_task_tcb->stack_pointer <= old_task_tcb->stack_overflow_address)
 		return false;
 	
-	// Set psp to new task
-	uint32_t new_task_psp = (uint32_t)new_task_tcb->stack_pointer;
+	// Set psp to new task (not necessary, done by restoreContext)
+	uint32_t new_task_psp = (uint32_t)new_task_tcb->stack_pointer; //
 	__set_PSP(new_task_psp); // CHECK THAT THIS ACTUALLY GETS SET
 	
 	// Restore context of new task from  stack
+	
+	volatile uint32_t testAgain = 0x77777777;
+	
 	restoreContext(new_task_psp);
+//	new_task_tcb->stack_pointer = (uint32_t*) __get_PSP; // not really necessary
+//	volatile uint32_t TEST = 0x33333333;
 	return true;
 }
 
 void SysTick_Handler(void) {
-	const uint32_t time_slice_len = 1000;
+	const uint32_t time_slice_len = 5000;
 	const uint32_t set_pendsv = (1<<28);
 	static uint32_t time_slice_count = time_slice_len;
 	
@@ -63,16 +70,46 @@ void SysTick_Handler(void) {
 }
 
 void PendSV_Handler(void){
-	const uint32_t clear_pendsv = (1<<27);
-
-	// Test context swtich
+	
+	// For testing context swtich
 	static uint8_t task_number = 0;
 	task_number %= 6;
 	
-	context_switch(&tcb_array[task_number], &tcb_array[task_number+1]);
+	// Store old task registers on task stack first thing before registers are messed with
+	tcb_array[task_number].stack_pointer = (uint32_t*)storeContext();
+	
+	const uint32_t clear_pendsv = (1<<27);
 	
 	// Remove pending state from pendSV exception
 	SCB->ICSR=(SCB->ICSR|clear_pendsv);
+	
+//	context_switch(&tcb_array[task_number], &tcb_array[task_number+1]);   Function call when implemented as a function
+	
+	
+//	tcb_t* old_task_tcb = &tcb_array[task_number];
+	tcb_t* new_task_tcb = &tcb_array[task_number+1];
+	
+	
+	volatile uint32_t tester = 0x99999999;
+	
+	// Check for overflow on old task
+//	if(old_task_tcb->stack_pointer <= old_task_tcb->stack_overflow_address)
+//		return false;
+	
+	// Set psp to new task (not necessary, done by restoreContext)
+//	uint32_t new_task_psp = (uint32_t)new_task_tcb->stack_pointer; //
+//	__set_PSP(new_task_psp); // CHECK THAT THIS ACTUALLY GETS SET
+	
+	// Restore context of new task from  stack
+	
+	volatile uint32_t testAgain = 0x77777777;
+	
+	restoreContext((uint32_t)new_task_tcb->stack_pointer);
+	
+//	new_task_tcb->stack_pointer = (uint32_t*) __get_PSP; // not really necessary, only need to save psp for task when not executing
+	volatile uint32_t TEST = 0x33333333;
+	task_number++;
+//	__set_PSP(__get_PSP()+(8*4));
 }
 
 
