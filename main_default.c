eb924216d8e630ca9fcce0e3050114a5b8fd5c43
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-18 20:33:44 -0400

    Work on mutex

--- main_default.c
+++ main_default.c
@@ -1,5 +1,4 @@
 #include <LPC17xx.h>
-#include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
@@ -8,6 +7,8 @@
 //extern tcb_t tcb_array[6];
 extern uint32_t msTicks;
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ SEMAPHORE TEST ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+/*
 semaphore_t blocker;
 
 void task1(void* arg){
@@ -83,7 +84,6 @@ void task5(void* arg){
 	}
 }
 
-
 int main(void) {
 	
 	volatile uint32_t test = 0x11111111;
@@ -101,6 +101,98 @@ int main(void) {
 //	task_create(task3, (void*)0x3, IDLE);
 //	task_create(task4, (void*)0x4, LOW);
 //	task_create(task5, (void*)0x5, LOW);
+
+//	uint32_t period = 1000; // 1s
+//	uint32_t prev = -period;
+	while(true) {
+//		if((uint32_t)(msTicks - prev) >= period){
+//			printf("tick ");
+//			prev += period;
+//		}
+		printf("t_main\n");
+	}
+}
+*/
+
+
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ MUTEX TEST ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void task1(void* arg){
+	static uint32_t delay_count = 0;
+	uint32_t print_val = (uint32_t) arg;
+	while(true) {
+		printf("%d\n", print_val);
+		delay_count++;
+		if(delay_count == 500){
+		}
+		if(delay_count == 1500){
+		}
+		if(delay_count == 2500){
+		}
+	}
+}
+
+void task2(void* arg){
+	static uint32_t delay_count = 0;
+	uint32_t print_val = (uint32_t) arg;
+	while(true) {
+		printf("%d\n", print_val);
+		delay_count++;
+		if(delay_count == 1000){
+		}
+		if(delay_count == 2000){
+		}
+	}
+}
+
+void task3(void* arg){
+	static uint32_t delay_count = 0;
+	uint32_t print_val = (uint32_t) arg;
+	while(true) {
+		printf("%d\n", print_val);
+		delay_count++;
+		if(delay_count == 1500){
+		}
+	}
+}
+
+void task4(void* arg){
+	static uint32_t delay_count = 0;
+	uint32_t print_val = (uint32_t) arg;
+	while(true) {
+		printf("%d\n", print_val);
+		delay_count++;
+		if(delay_count == 2000){
+		}
+	}
+}
+
+void task5(void* arg){
+	static uint32_t delay_count = 0;
+	uint32_t print_val = (uint32_t) arg;
+	while(true) {
+		printf("%d\n", print_val);
+		delay_count++;
+		if(delay_count == 2500){
+		}
+	}
+}
+
+int main(void) {
+	
+	volatile uint32_t test = 0x11111111;
+	
+	printf("\nStarting...\n\n");
+	SysTick_Config(SystemCoreClock/1000);
+	printf("1\n");
+	
+	rtos_init();
+
+	task_create(task1, (void*)0x1, LOW);
+	task_create(task2, (void*)0x2, IDLE);
+	task_create(task3, (void*)0x3, IDLE);
+	task_create(task4, (void*)0x4, LOW);
+	task_create(task5, (void*)0x5, LOW);
 
 //	uint32_t period = 1000; // 1s
 //	uint32_t prev = -period;
