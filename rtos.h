4d7bfbe08185996afe5b861d3f911f7a20f5fa5e
julianlowery <37548369+julianlowery@users.noreply.github.com>
2019-07-20 16:32:47 -0400

    Finished testing, added demos, cleaned code

--- rtos.h
+++ rtos.h
@@ -8,10 +8,12 @@
 void rtos_init(void);
 void task_create(rtosTaskFunc_t function_pointer, void* function_arg, priority_t task_priority);
 
+// Semaphore interface
 void semaphore_init(semaphore_t *sem, uint8_t initial_count);
 void semaphore_take(semaphore_t *sem);
 void semaphore_give(semaphore_t *sem);
 
+// Mutex interface
 void mutex_init(mutex_t *mutex);
 void mutex_give(mutex_t *mutex);
 void mutex_take(mutex_t *mutex);
